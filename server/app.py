28ad05c05460ad78c8e3ed8a9db868c4c74a8f00
gokkuu100 <wkurts247@gmail.com>
2023-10-10 13:48:48 +0300

    compplete

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 from models import db, Plant
 
@@ -16,12 +16,57 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        plant_list = []
+        for plant in plants:
+            plant_data = {
+                'id': plant.id,
+                'name': plant.name,
+                'image': plant.image,
+                'price': plant.price
+            }
+            plant_list.append(plant_data)
+        return jsonify(plant_list)
+
+    def post(self):
+        data = request.json
+        name = data.get('name')
+        image = data.get('image')
+        price = data.get('price')
+
+        if name is None or image is None or price is None:
+            return {'error': 'Missing required fields'}, 400
+
+        new_plant = Plant(
+            name=name,
+            image=image,
+            price=price
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return {
+            'id': new_plant.id,
+            'name': new_plant.name,
+            'image': new_plant.image,
+            'price': new_plant.price
+        }, 201
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.get_or_404(id)
+        plant_data = {
+            'id': plant.id,
+            'name': plant.name,
+            'image': plant.image,
+            'price': plant.price
+        }
+        return jsonify(plant_data)
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
